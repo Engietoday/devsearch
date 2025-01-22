3fe10d26cad75c39acfb96b4b77610e2d5e35a88
Ao Yu <engietoday@gmail.com>
2025-01-21 23:14:18 -0500

    update

--- users/views.py
+++ users/views.py
@@ -14,7 +14,7 @@ def loginUser(request):
         return redirect('profiles')
     
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         
         try:
@@ -26,7 +26,7 @@ def loginUser(request):
         
         if user is not None:
             login(request, user)
-            return redirect('profiles')
+            return redirect(request.GET['next'] if 'next' in request.GET else 'account')
         else:
             messages.error(request, 'Username OR password is incorrect')
 
